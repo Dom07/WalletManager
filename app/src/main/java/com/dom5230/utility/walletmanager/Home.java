51cbc2b7bb655efc6f6c065202737da66106dee9
Dominic <dominic5230@gmail.com>
2018-05-23 16:34:27 +0530

    Checked the app with mpchart and graph view, but unfortunately removed them due to some gradle error, which got fixed when i restarted studio. wew.

--- app/src/main/java/com/dom5230/utility/walletmanager/Home.java
+++ app/src/main/java/com/dom5230/utility/walletmanager/Home.java
@@ -1,10 +1,7 @@
 package com.dom5230.utility.walletmanager;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -12,20 +9,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class Home extends Fragment {
 
     // Objects
-    private MySqliteTaskHelper helper;
+    MySqliteTaskHelper helper;
+    TransactionHistoryAdapter historyAdapter;
+    ArrayList<TransactionRecord> items;
 
     // UI Components
     FloatingActionButton fab;
@@ -45,32 +40,46 @@ public class Home extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
-                View mview = getLayoutInflater().inflate(R.layout.expense_input_dailogue,null);
-                final EditText ETAmount = mview.findViewById(R.id.ETAmount);
-                final Spinner spinner = mview.findViewById(R.id.SpinnerCategory);
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.category, android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
-
-                mBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        helper.insertRow(getContext(), ETAmount.getText().toString(),spinner.getSelectedItem().toString());
-                    }
-                });
-                mBuilder.setNegativeButton("Cancel", null);
-                mBuilder.setView(mview);
-                mBuilder.setTitle("Expenditure Details");
-                AlertDialog dialog = mBuilder.create();
-                dialog.show();
+                expenseAlertBox();
             }
         });
 
-        ArrayList<TransactionHistoryItem> items = helper.getRowsAsArrayListObjects(getContext());
-        TransactionHistoryAdapter adapter1 = new TransactionHistoryAdapter(getActivity(), items);
-        lvLastFiveTransactions.setAdapter(adapter1);
+        populateListView();
 
         return view;
+    }
+
+
+    public void expenseAlertBox(){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+        View mview = getLayoutInflater().inflate(R.layout.expense_input_dailogue,null);
+
+        final EditText ETAmount = mview.findViewById(R.id.ETAmount);
+        final Spinner spinner = mview.findViewById(R.id.SpinnerCategory);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.category, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        mBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                helper.insertRow(getContext(), ETAmount.getText().toString(),spinner.getSelectedItem().toString());
+                populateListView();
+
+            }
+        });
+
+        mBuilder.setNegativeButton("Cancel", null);
+        mBuilder.setView(mview);
+        mBuilder.setTitle("Expenditure Details");
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+    }
+
+    public void populateListView(){
+        items = helper.getRecentTwoRows(getContext());
+        historyAdapter = new TransactionHistoryAdapter(getActivity(), items);
+        lvLastFiveTransactions.setAdapter(historyAdapter);
     }
 }
